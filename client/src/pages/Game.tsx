56812d4831d776c12a71427157a470113ec4dffa
rickmoe <Cosmic.Star@comcast.net>
2023-07-29 23:48:24 -0500

    Fixed display for various heights

--- client/src/pages/Game.tsx
+++ client/src/pages/Game.tsx
@@ -6,14 +6,25 @@ import Keyboard from "../components/Keyboard";
 import Scoreboard from "../components/Scoreboard";
 import "./Game.css";
 
+const getWordLength = (
+  searchParams: URLSearchParams,
+  min: number,
+  max: number
+): number => {
+  const wordLengthString = searchParams.get("word-length");
+  const wordLength = wordLengthString == null ? 5 : parseInt(wordLengthString);
+  if (wordLength < min) return min;
+  if (wordLength > max) return max;
+  return wordLength;
+};
+
 interface GameProps {
   mode: GameMode;
 }
 
 const Game = ({ mode }: GameProps) => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const wordLength = parseInt(searchParams.get("word-length") ?? "5");
-  console.log(wordLength);
+  const wordLength = getWordLength(searchParams, 4, 6);
 
   const { tileData, handleInput, gameState, score } = useGameState(
     mode,
