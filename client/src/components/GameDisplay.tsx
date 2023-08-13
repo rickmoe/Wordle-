0dda3ad284a84762193fb0b5e167816df1826d99
rickmoe <Cosmic.Star@comcast.net>
2023-08-12 23:29:51 -0500

    Split game display into columns for short words

--- client/src/components/GameDisplay.tsx
+++ client/src/components/GameDisplay.tsx
@@ -2,19 +2,41 @@ import { TileData } from "../types";
 import GameTile from "./GameTile";
 import "./GameDisplay.css";
 
+const splitToColumns = (tileData: TileData[][]): TileData[][][] => {
+  if (tileData[0].length > 2) return [tileData];
+  // Split small word lengths into columns for better visibility
+  const midIndex = Math.ceil(tileData.length / 2);
+  const column1 = tileData.slice(0, midIndex);
+  const column2 = tileData
+    .slice(midIndex)
+    .concat(tileData.length % 2 ? [[]] : []);
+  return [column1, column2];
+};
+
 interface GameDisplayProps {
   tileData: TileData[][];
 }
 
-const GameDisplay = (props: GameDisplayProps) => {
+const GameDisplay = ({ tileData }: GameDisplayProps) => {
+  const displayColumns = splitToColumns(tileData);
+  const isSplit = displayColumns.length > 1;
+
   return (
     <section className="game-display">
-      {props.tileData.map((tileRow, rowNum) => (
-        <div key={rowNum} className="tile-row">
-          {tileRow.map((tile, colNum) => (
-            <GameTile key={colNum} letter={tile.letter} result={tile.result} />
+      {displayColumns.map((columnData) => (
+        <section className={"game-display-column" + (isSplit ? " split" : "")}>
+          {columnData.map((tileRow, rowNum) => (
+            <div key={rowNum} className="tile-row">
+              {tileRow.map((tile, colNum) => (
+                <GameTile
+                  key={colNum}
+                  letter={tile.letter}
+                  result={tile.result}
+                />
+              ))}
+            </div>
           ))}
-        </div>
+        </section>
       ))}
     </section>
   );
